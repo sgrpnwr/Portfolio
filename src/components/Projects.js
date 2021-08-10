3ee608575a224ead737e77e3e1d206856b80c650
Sagar Panwar <sagarpanwar@Sagars-MacBook-Air.local>
2021-08-10 21:57:57 +0530

    fixed scroll-position whhen we change links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,6 @@
 import styles from "./Projects.module.css";
 import Project from "./Project"
 
-const info="I'm a paragraph. Click here to add your own text and edit me. Its easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you."
 const allProjects=[
 
 {
