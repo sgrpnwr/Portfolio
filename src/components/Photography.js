ca6a72248455c020621e6ee6e002364b0a964177
Sagar Panwar <sagarpanwar@Sagars-MacBook-Air.local>
2021-08-10 20:18:19 +0530

    fixed navbar padding and photography links size

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -8,7 +8,7 @@ const Photography=()=>{
     <form target="_blank" action="https://vsco.co/sagarpanwar/gallery">
   
     <button class="card 1">
-      <div class="card_image"> <img src="https://play-lh.googleusercontent.com/VrSAPsppGQ82MFcP-aVIB38ZgT7XOFs9jZC7xGQPaDuCy637Tz4NCn4-NsKyKs_hfZQ" /> </div>
+      <div class="card_image"> <img src="https://play-lh.googleusercontent.com/VrSAPsppGQ82MFcP-aVIB38ZgT7XOFs9jZC7xGQPaDuCy637Tz4NCn4-NsKyKs_hfZQ" /><h3> VSCO</h3></div>
       <div class="card_title title-white">
       
       </div>
@@ -16,7 +16,7 @@ const Photography=()=>{
     <form target="_blank" action="https://www.instagram.com/sagar_panwar/">
       <button class="card 2">
       <div class="card_image">
-        <img src="https://cdn.digitbin.com/wp-content/uploads/Instagram-Lite.jpg" />
+        <img src="https://cdn.digitbin.com/wp-content/uploads/Instagram-Lite.jpg" />    <h3>Instagram</h3>
         </div>
       <div class="card_title title-white">
      
@@ -29,7 +29,7 @@ const Photography=()=>{
     
     <button class="card 3">
       <div class="card_image">
-        <img src="https://cdn.iconscout.com/icon/free/png-512/behance-41-114718.png" />
+        <img src="https://cdn.iconscout.com/icon/free/png-512/behance-41-114718.png" />   <h3>Behance</h3>
       </div>
       <div class="card_title">
       
